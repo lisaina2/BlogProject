f9fd0c3018aad9fd122ef671b05e056475e47d98
lisaina2 <lisaina2@yandex.ru>
2025-11-01 11:47:49 +0000

    внесен mysql

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db import models  # Импорт модуля models из Django
 
-# Create your models here.
+# Create your models here.  # Комментарий — указание, где создавать модели
+
+class Post(models.Model):  # Объявление класса модели Post
+    title = models.CharField(max_length=100)  # Поле «название» (до 100 символов)
+    content = models.TextField()  # Поле «содержание» (текст без ограничения длины)
+    author = models.CharField(max_length=100)  # Поле «автор» (до 100 символов)
+    created_at = models.DateTimeField(auto_now_add=True)  (автоматически заполняется при создании записи)
